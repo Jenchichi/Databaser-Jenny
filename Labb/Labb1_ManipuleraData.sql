db2066fe0cac47d21738d5d77f7ecbab2dad5f6b
Jenny Skoglund <jenny.skoglund@iths.se>
2025-05-12 16:11:32 +0200

    update

---
+++ Labb/Labb1_ManipuleraData.sql
@@ -0,0 +1,110 @@
+-- SELECT * FROM MoonMissions;
+
+SELECT
+    Spacecraft,
+    [Launch date],
+    [Carrier rocket],
+    Operator,
+    [Mission type]
+INTO SuccessfulMissions
+FROM MoonMissions
+WHERE Outcome = 'Successful';
+
+GO
+
+UPDATE SuccessfulMissions
+SET [Operator] = LTRIM([Operator]); 
+
+GO
+
+UPDATE SuccessfulMissions
+SET Spacecraft = LTRIM(LEFT(Spacecraft, CHARINDEX('(', Spacecraft + '()') -1));
+
+GO
+
+SELECT
+    Operator,
+    [Mission type],
+    COUNT(*) AS [Mission count]
+FROM 
+    SuccessfulMissions
+GROUP BY 
+    Operator,
+    [Mission type]
+HAVING
+    COUNT(*) > 1
+ORDER BY
+    Operator,
+    [Mission type];
+
+GO
+
+SELECT 
+    ID,
+    UserName,
+    Password,
+    Email,
+    Phone,
+    FirstName + ' ' + LastName AS Name,
+    CASE
+        WHEN CAST(SUBSTRING(ID, 10, 1) AS INT) % 2 = 0 THEN 'Female'
+        ELSE 'Male'
+    END AS Gender
+INTO NewUsers
+FROM Users;
+
+GO
+
+SELECT
+    UserName,
+    COUNT(*) AS DublicateCount
+FROM
+    NewUsers
+GROUP BY
+    UserName
+HAVING COUNT(*) > 1;
+
+GO
+
+WITH Duplicates AS (
+    SELECT
+        ID,
+        UserName,
+        ROW_NUMBER() OVER (PARTITION BY UserName ORDER BY ID) AS RowNum
+    FROM NewUsers
+)
+UPDATE NewUsers
+SET UserName = CONCAT(LEFT(Duplicates.UserName, 50), Duplicates.RowNum)
+FROM NewUsers
+JOIN Duplicates ON NewUsers.ID = Duplicates.ID
+AND Duplicates.RowNum > 1;
+
+GO
+
+DELETE FROM NewUsers
+WHERE Gender = 'Female'
+AND CAST(LEFT(ID, 2) AS INT) < 70;
+
+GO
+
+INSERT INTO NewUsers (
+    ID, UserName, Password, Email, Phone, Name, Gender
+)
+VALUES (
+    '900324-1243',
+    'karsko',
+    'password123',
+    'karolina.skoglund@test.com',
+    '0701234567',
+    'Karolina Skoglund',
+    'Female'
+);
+
+GO
+
+
+
+
+
+
+
